eef3da23b154738ed54706c16d210cead7d8b556
ceewaigithub <chongyong321@gmail.com>
2024-02-02 11:09:30 +0800

    Update compiled Java files and fix Main class path

--- compile_run.bat
+++ compile_run.bat
@@ -5,12 +5,8 @@ mkdir class
 
 REM Compile the Java files
 echo Compiling Java files...
-javac -d class src/**/*.java
-
-REM List the contents of the class directory
-REM echo Contents of the class directory:
-REM dir /s class
+javac -d class src\**\*.java
 
 REM Run the Main class
 echo Running project...
-java -cp class Main
+java -cp class world.Main
