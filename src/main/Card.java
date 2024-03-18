5d734fb7185fcf7f97ff55a046af747d6259df66
saracyh <sarachen04@gmail.com>
2024-03-18 15:11:15 +0800

    Added TODOs to align my Card.java with Adrian's

--- src/main/Card.java
+++ src/main/Card.java
@@ -6,10 +6,14 @@ public class Card {
     private String suit;
     private String rank;    
 
+    // Insert cardWidth and height
+
     public Card(String suit, String rank) {
         this.suit = suit;
         this.rank = rank;
     }
+
+    // Insert constructor 2
 
     // Getters and setters
     public String getRank() {
@@ -38,13 +42,17 @@ public class Card {
         }
     }
 
+    // Add into Adrian's
     public boolean isAce() {
         return rank.equals("a");
     }
-
+    
+    // Replace with getImage
     public String getImagePath() {
         return "./cards/" + rank + suit + ".gif";
     }
+
+    // Add Print card, Get card width, Get card height
     @Override
     public String toString() {
         return rank + "" + suit;
