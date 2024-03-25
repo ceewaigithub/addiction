b7ffdd845218a1fb4b9598a6ad6dd434aad17141
ceewaigithub <chongyong321@gmail.com>
2024-03-25 12:15:22 +0800

    Add directory creation for compiled Java files

--- compile_run.bat
+++ compile_run.bat
@@ -1,12 +1,13 @@
 @echo off
 
 REM Create the class directory if it doesn't exist
-mkdir class
+if not exist class mkdir class
 
 REM Compile all Java files in the res folder
 echo Compiling Java files...
 REM Compile all Java files in the src folder and its subdirectories
 for /r src %%G in (*.java) do (
+    if not exist class\%%~dpG mkdir class\%%~dpG
     javac -d class -sourcepath src "%%G"
 )
 
