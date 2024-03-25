513dc26a9585bc8287234f1b04e44f879ce2e908
ceewaigithub <chongyong321@gmail.com>
2024-03-25 12:18:00 +0800

    Refactor compile_run.bat to improve Java file compilation

--- compile_run.bat
+++ compile_run.bat
@@ -3,12 +3,11 @@
 REM Create the class directory if it doesn't exist
 if not exist class mkdir class
 
-REM Compile all Java files in the res folder
-echo Compiling Java files...
 REM Compile all Java files in the src folder and its subdirectories
-for /r src %%G in (*.java) do (
-    if not exist class\%%~dpG mkdir class\%%~dpG
-    javac -d class -sourcepath src "%%G"
+echo Compiling Java files...
+for /r src %%F in (*.java) do (
+    echo Compiling %%F...
+    javac -d class -cp class "%%F"
 )
 
 REM Run the Main class
