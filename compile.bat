4d9a83b757cbe09cc6fae75b7f72c173272a2c53
ChampionGH <158540713+ChampionGH@users.noreply.github.com>
2024-03-26 21:50:45 +0800

    Typo in compile.bat file

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Create the class directory if it doesn't exist
-if not exist class mkdir classs
+if not exist class mkdir class
 
 REM Compile all Java files in the src folder and its subdirectories
 echo Compiling Java files...
