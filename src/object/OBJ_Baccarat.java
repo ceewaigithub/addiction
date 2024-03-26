0ad02b6c29ddc68648871e9c8d8d10cfb1b03878
ceewaigithub <chongyong321@gmail.com>
2024-03-26 17:44:41 +0800

    Update image file paths in OBJ_Baccarat, OBJ_BlackJack, and OBJ_HighLow

--- src/object/OBJ_Baccarat.java
+++ src/object/OBJ_Baccarat.java
@@ -10,9 +10,9 @@ public class OBJ_Baccarat extends SuperObject {
         collision = true;
         String currentDirectory = new File("").getAbsolutePath();
         try {
-            image = ImageIO.read(new File(currentDirectory + "/res/objects/chest.png"));
+            image = ImageIO.read(new File(currentDirectory + "/res/objects/table.png"));
         } catch (Exception e) {
-            System.out.println("Directory" + currentDirectory + "/res/objects/chest.png");
+            System.out.println("Directory" + currentDirectory + "/res/objects/table.png");
             e.printStackTrace();
         }
     }
