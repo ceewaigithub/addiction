edf0c6671105e0fab68fba07ecbb7b46e92f2ec8
ceewaigithub <chongyong321@gmail.com>
2024-03-30 22:44:25 +0800

    Add documentation for NewGameException and Interactable interface

--- src/main/NewGameException.java
+++ src/main/NewGameException.java
@@ -1,5 +1,9 @@
 package main;
 
+/**
+ * This exception is used to indicate an error when starting a new game.
+ * It allows the program to handle specific errors related to starting a new game.
+ */
 public class NewGameException extends Exception{
     public NewGameException(String message){
         super(message);
--- src/object/Interactable.java
+++ src/object/Interactable.java
@@ -3,6 +3,16 @@ package object;
 import entity.User;
 import world.GamePanel;
 
+/**
+ * The Interactable interface represents an object that can be interacted with in the game.
+ * Any class that implements this interface must provide an implementation for the interact method.
+ */
 public interface Interactable {
+    /**
+     * Performs an interaction between the specified user and the game panel.
+     * 
+     * @param user The user performing the interaction.
+     * @param gp The game panel where the interaction takes place.
+     */
     void interact(User user, GamePanel gp);
 }
