806652dbb1be3160d0a301bab806d8bd8fd6d9f8
ceewaigithub <chongyong321@gmail.com>
2024-03-31 18:10:10 +0800

    Add Player class with methods and Javadoc comments

--- src/card/Player.java
+++ src/card/Player.java
@@ -3,30 +3,56 @@ package card;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Player class represents a player in a card game.
+ */
 public class Player {
     private String name;
     private List<Card> hand;
-
     private int money = 1000;
 
-
+    /**
+     * Constructs a Player object with the specified name.
+     *
+     * @param name the name of the player
+     */
     public Player(String name) {
         this.name = name;
         this.hand = new ArrayList<>();
     }
 
+    /**
+     * Returns the name of the player.
+     *
+     * @return the name of the player
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the hand of the player.
+     *
+     * @return the hand of the player
+     */
     public List<Card> getHand() {
         return hand;
     }
 
+    /**
+     * Adds a card to the player's hand.
+     *
+     * @param card the card to be added
+     */
     public void addCard(Card card) {
         hand.add(card);
     }
 
+    /**
+     * Calculates and returns the value of the player's hand.
+     *
+     * @return the value of the player's hand
+     */
     public int getHandValue() {
         int value = 0;
         for (Card card : hand) {
@@ -35,25 +61,43 @@ public class Player {
         return value;
     }
 
+    /**
+     * Discards all cards from the player's hand.
+     */
     public void discardHand() {
         hand.clear();
     }
 
-    public boolean isDealer(){
-        if(name == "Dealer"){
-            return true;
-        }
-        return false;
+    /**
+     * Checks if the player is a dealer.
+     *
+     * @return true if the player is a dealer, false otherwise
+     */
+    public boolean isDealer() {
+        return name.equals("Dealer");
     }
 
-    public int getBalance(){
+    /**
+     * Returns the balance (money) of the player.
+     *
+     * @return the balance of the player
+     */
+    public int getBalance() {
         return money;
     }
 
-    public void setBalance(int toSet){
+    /**
+     * Sets the balance (money) of the player.
+     *
+     * @param toSet the balance to be set
+     */
+    public void setBalance(int toSet) {
         money = toSet;
     }
 
+    /**
+     * Prints the player's hand.
+     */
     public void printHand() {
         System.out.print(name + "'s Hand: [");
         for (int i = 0; i < hand.size(); i++) {
@@ -67,8 +111,10 @@ public class Player {
         System.out.println(name + "'s Hand Value: " + getHandValue());
     }
 
+    /**
+     * Prints the size of the player's hand.
+     */
     public void printHandSize() {
         System.out.println(name + "'s Hand Size: " + hand.size());
     }
-
 }
--- src/exception/DeckEmptyException.java
+++ src/exception/DeckEmptyException.java
@@ -1,5 +1,8 @@
 package exception;
 
+/**
+ * This class represents a custom exception that is thrown when a deck is empty.
+ */
 public class DeckEmptyException extends RuntimeException {
     public DeckEmptyException(String message) {
         super(message);
