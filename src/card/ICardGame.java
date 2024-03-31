67866727671bffdb19195e7f3c711d80ccc94db9
ceewaigithub <chongyong321@gmail.com>
2024-03-31 18:10:54 +0800

    Add startGame() and endRound() methods to ICardGame interface

--- src/card/ICardGame.java
+++ src/card/ICardGame.java
@@ -1,6 +1,17 @@
 package card;
 
+/**
+ * This interface represents a card game.
+ */
 public interface ICardGame {
+    
+    /**
+     * Starts the game.
+     */
     void startGame();
+    
+    /**
+     * Ends the current round of the game.
+     */
     void endRound();
 }
