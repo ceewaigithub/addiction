dd06c20a5a48617fa981f4f4d39572d7786b6a1d
saracyh <sarachen04@gmail.com>
2024-03-31 15:18:13 +0800

    Created cardsaraver

---
+++ cardsaraver/CardGame.java
@@ -0,0 +1,32 @@
+package cardsaraver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.BettingSystem;
+import main.Deck;
+import main.Player;
+
+public abstract class CardGame {
+    
+    private Deck deck;
+    private List<Player> players;
+    private BettingSystem bettingSystem;
+
+    public CardGame(BettingSystem bettingSystem) {
+        this.bettingSystem = bettingSystem;
+        players = new ArrayList<>();
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public abstract void startGame();
+
+    public void endGame() {
+        for (Player player : players) {
+            player.discardHand();
+        }
+    }
+}
